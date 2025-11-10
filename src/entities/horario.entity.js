753391b3fea167a0b9550f3908d42b774ebad7c6
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-11-10 01:51:52 -0300

    horario
    entidades y servicio

---
+++ src/entities/horario.entity.js
@@ -0,0 +1,21 @@
+
+let horariosDB = [];
+const generarId = () => 't-' + Math.random().toString(36).substring(2, 6);
+
+
+class HorarioEntity {
+    constructor({ fecha, hora, duracionMinutos }) {
+        this.id = generarId();
+        this.fecha = fecha;
+        this.hora = hora;
+        this.duracionMinutos = parseInt(duracionMinutos);
+        this.disponible = true; 
+        this.estudianteId = null; 
+    }
+}
+
+
+module.exports = {
+    HorariosDB: horariosDB,
+    HorarioEntity,
+};
