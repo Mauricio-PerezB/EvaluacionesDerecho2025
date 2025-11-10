13283d02a2cc8ff40cf2d16f1475f933065cdd28
Bryan Suazo <bryan.suazo2301@alumnos.ubiobio.cl>
2025-11-09 23:19:29 -0300

    Mejora y Creacion
    Mejora y Creacion del middleware y handlers

--- src/Handlers/responseHandlers.js
+++ src/Handlers/responseHandlers.js
@@ -1,6 +1,6 @@
 "use strict";
 
-export const handleSuccess = (res, statusCode, message, data = null) => {
+export const handleSuccess = (res, statusCode = 200, message, data = null) => {
   res.status(statusCode).json({
     message,
     data,
@@ -16,11 +16,20 @@ export const handleErrorClient = (res, statusCode, message, errorDetails = null)
   });
 };
 
-export const handleErrorServer = (res, statusCode, message, errorDetails = null) => {
+export const handleErrorServer = (res, statusCode = 500, message, errorDetails = null) => {
   console.error("Server Error:", message, errorDetails);
-  res.status(statusCode).json({
-    message,
-    errorDetails,
-    status: "Server error",
-  });
+
+  if (process.env.NODE_ENV === 'production') {
+    res.status(500).json({
+      message: "Ocurrió un error inesperado en el servidor.",
+      errorDetails: null,
+      status: "Server error",
+    });
+  } else {
+    res.status(statusCode).json({
+      message,
+      errorDetails: errorDetails || "No details provided.",
+      status: "Server error",
+    });
+  }
 };
