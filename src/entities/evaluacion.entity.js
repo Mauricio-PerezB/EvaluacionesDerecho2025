a54040f5c22642ef5cea7a16c5b5855e7648a4d0
VicenteUlloa <vicenteu605@gmail.com>
2025-11-09 23:13:52 -0300

    evaluacion
    entidad evaluacion

---
+++ src/entities/evaluacion.entity.js
@@ -0,0 +1,48 @@
+import { EntitySchema } from "typeorm";
+
+export const EvaluacionSchema = new EntitySchema({
+    name: "Evaluacion", 
+    tableName: "evaluaciones", 
+    columns: {
+        id: {
+            type: "int",
+            primary: true,
+            generated: "increment",
+        },
+        nombre: {
+            type: "varchar",
+            nullable: false,
+        },
+        fechaRealizacion: {
+            name: "fecha_realizacion",
+            type: "timestamp",
+            nullable: false,
+        },
+        porcentajeRamo: {
+            name: "porcentaje_ramo",
+            type: "decimal",
+            precision: 5,
+            scale: 2,
+            nullable: false,
+        },
+        createdAt: {
+            name: "created_at",
+            type: "timestamp",
+            createDate: true,
+        },
+        updatedAt: {
+            name: "updated_at",
+            type: "timestamp",
+            updateDate: true,
+        },
+    },
+    relations: {
+        ramo: {
+            target: "Ramo",
+            type: "many-to-one",
+            joinColumn: { name: "ramo_id" },
+            inverseSide: "evaluaciones",
+            nullable: false,
+        },
+    },
+});
